87f50329776e07ecbd47f2843d53152a9dc6e5c1
Algernon <jlgw.stuff@gmail.com>
2016-12-29 16:59:05 +0800

    Allowed reusability

--- src/split_flowers/Flower_1.java
+++ src/split_flowers/Flower_1.java
@@ -1,12 +1,14 @@
 package split_flowers;
 
 import java.awt.*;
-
 import javax.swing.*;
 
-public class Flower_1 {
-	public static void main(String[] args) {
-		JFrame fr = new JFrame();
+public class Flower_1 extends JFrame{
+	private static final long serialVersionUID = 3752120557796563206L;
+
+	public Flower_1() {
+		super("Flower_1");
+		
 		JPanel jp = new JPanel();
 		LayoutManager overlay = new OverlayLayout(jp);
 		jp.setLayout(overlay);
@@ -15,10 +17,14 @@ public class Flower_1 {
 		jp.add(new Middle(Color.BLACK,50,100,10));
 		jp.add(new Middle(Color.BLACK,50,150,10));
 		jp.add(new Middle(Color.BLACK,50,200,10));
-		fr.add(jp);
-		fr.setSize(600, 600);
-        fr.setVisible(true);
-        fr.setResizable(false);
-        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		add(jp);
+	}
+	
+	public static void main(String[] args) {
+		Flower_1 fl1 = new Flower_1();
+		fl1.setSize(600, 600);
+        fl1.setVisible(true);
+        fl1.setResizable(false);
+        fl1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
