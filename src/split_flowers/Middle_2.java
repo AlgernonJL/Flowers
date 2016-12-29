4d6be0354042019d90d98d829c8fd79119bd6623
Algernon <jlgw.stuff@gmail.com>
2016-12-29 17:21:02 +0800

    added simpler middlebit

---
+++ src/split_flowers/Middle_2.java
@@ -0,0 +1,30 @@
+package split_flowers;
+
+import java.awt.*;
+import java.awt.geom.*;
+import javax.swing.*;
+
+public class Middle_2 extends JPanel{
+	int centX;
+	int centY;
+	Color clr;
+	int rad;
+	
+	public Middle_2(Color clr, int rad){
+		this.clr = clr;
+		this.rad = rad;
+		setOpaque(false);
+	}
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		
+		centX = getSize().width/2;
+		centY = getSize().height/2;
+		
+		g2.setPaint(clr);
+		g2.fill(new Ellipse2D.Double(centX-rad/2, centY-rad/2, rad,rad));
+		
+	}
+}
