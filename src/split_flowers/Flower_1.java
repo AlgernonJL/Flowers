71fb2c1f6a6d540c3cf2fd6c368001cdc07b7793
Algernon <jlgw.stuff@gmail.com>
2016-12-29 16:53:01 +0800

    Made flower look a bit nicer

--- src/split_flowers/Flower_1.java
+++ src/split_flowers/Flower_1.java
@@ -10,9 +10,11 @@ public class Flower_1 {
 		JPanel jp = new JPanel();
 		LayoutManager overlay = new OverlayLayout(jp);
 		jp.setLayout(overlay);
-		
-		jp.add(new Middle(Color.BLACK,50,200,5));
-		jp.add(new Middle(Color.BLACK,50,20,10));
+
+		jp.add(new Middle(Color.BLACK,50,50,10));
+		jp.add(new Middle(Color.BLACK,50,100,10));
+		jp.add(new Middle(Color.BLACK,50,150,10));
+		jp.add(new Middle(Color.BLACK,50,200,10));
 		fr.add(jp);
 		fr.setSize(600, 600);
         fr.setVisible(true);
